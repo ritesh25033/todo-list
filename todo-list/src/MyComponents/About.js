edc9021ea158f6b14aa896ac6c128784f7d0dc12
Ritesh Agrahari <60784540+ritesh25033@users.noreply.github.com>
2022-10-02 14:45:35 +0530

    second commit

---
+++ todo-list/src/MyComponents/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+    <div>
+      {
+        <ul>
+          <br/>
+            This is an about component
+          <br/>
+          <li>In this react app you can add your todos and delete it also</li>
+          <br/>
+          <li>Uses CRUD Operations</li>
+        </ul>
+      }
+    </div>
+  )
+}
